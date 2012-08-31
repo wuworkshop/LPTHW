b56405ac4a0b45d853f6941634ea3fe80077e596
wuworkshop <imouto512@gmail.com>
2012-08-31 01:05:11 -0400

    Exercise 3 plus Extra Credit (marked as -ec) #s 1, 3, and 5.

---
+++ ex3/ex3-ec1.py
@@ -0,0 +1,42 @@
+﻿# Prints 'I will now count my chickens:'
+print "I will now count my chickens:"
+
+# Prints 'Hens 30'
+print "Hens", 25 + 30 / 6
+
+# Prints 'Roosters 97'
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints 'Now I will count the eggs:'
+print "Now I will count the eggs:"
+
+# Prints '7' even though it should be 6.75. This is due to how 
+# computers handle fractions - see floating-point-gui.de for explanation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints 'Is it true that 3 + 2 < 5 - 7?'
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints 'False'
+print 3 + 2 < 5 - 7
+
+# Prints 'What is 3 + 2? 5'
+print "What is 3 + 2?", 3 + 2
+
+# Prints 'What is 5 - 7? -2'
+print "What is 5 - 7?", 5 - 7
+
+# Prints 'Oh, that's why it's False.'
+print "Oh, that's why it's False."
+
+# Prints 'How about some more.'
+print "How about some more."
+
+# Prints 'Is it greater? True'
+print "Is it greater?", 5 > -2
+
+# Prints 'It is greater or equal? True'
+print "It is greater or equal?", 5 >= -2
+
+# Prints 'Is it less or equal? False'
+print "Is it less or equal?", 5 <= -2
