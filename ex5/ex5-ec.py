d0fe5cf99091619eb46ec80d4a5d495b9dcf05ff
wuworkshop <imouto512@gmail.com>
2012-08-31 03:53:44 -0400

    Exercise 5 plus Extra Credit (marked as -ec).

---
+++ ex5/ex5-ec.py
@@ -0,0 +1,22 @@
+﻿name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_in_cm = height * 2.54 # 1 inch = 2.54 cm
+weight_in_kg = weight * 0.4536 # 1 lb = 0.4536 kg 
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+
+print "His weight in kilos is %r." % weight_in_kg
+print "His height in centimeters is %g." % height_in_cm
