c6e81fc9038ca41b4d98653893a845136f103687
wuworkshop <imouto512@gmail.com>
2012-09-03 05:06:28 -0400

    Exercise 11 plus the Extra Credit.

---
+++ ex11/ex11.py
@@ -0,0 +1,21 @@
+﻿print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# Extra Credit
+s = raw_input('--> '),
+print s
+
+print "What's your first name?",
+firstname = raw_input()
+print "What's your last name?",
+lastname = raw_input()
+print "What's your email address?",
+email = raw_input()
+
+print "Thank you for registering %s %s. We sent a confirmation email to %s." % (firstname, lastname, email)
